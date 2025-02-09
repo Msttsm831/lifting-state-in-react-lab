ff3a5f7cfbaae9a93d42ffbc6f78ef0e20f54c6a
msttsm831 <Mohdthamer02@gmail.com>
2025-02-09 22:57:29 +0300

    Burger

---
+++ src/components/BurgerStack/BurgerStack.jsx
@@ -0,0 +1,18 @@
+// src/components/BurgerStack/BurgerStack.jsx
+import React from 'react';
+
+const BurgerStack = ({ stack, removeFromBurger }) => {
+  return (
+    <ul>
+      {stack.map((ingredient, index) => (
+        <li key={index} style={{ backgroundColor: ingredient.color }}>
+          {ingredient.name}
+          <button onClick={() => removeFromBurger(index)}>X</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default BurgerStack;
+
